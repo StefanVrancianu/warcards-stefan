76b0116495f9f7b2618727d888f08411bc2aed81
Stefan Vrancianu <stefanvrancianu@stefans-macbook-pro.local>
2015-12-16 20:31:46 +0200

    Initial Commit

---
+++ WarCards/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WarCards
+//
+//  Created by Stefan Vrancianu on 16/12/15.
+//  Copyright © 2015 Stefan Vrancianu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WarCards/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WarCards
+//
+//  Created by Stefan Vrancianu on 16/12/15.
+//  Copyright © 2015 Stefan Vrancianu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
